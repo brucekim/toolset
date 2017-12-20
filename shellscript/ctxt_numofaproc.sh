4e4c4cbe1f83177e5f7b33378efc0d8dd28d2a7b
Bruce Kim <bruce.kim.it@gmail.com>
2017-12-20 10:00:08 +0900

    Measure the number of context switch for a process
    A pid is given as parameter, then it measures the number of context switch

---
+++ shellscript/ctxt_numofaproc.sh
@@ -0,0 +1,18 @@
+tmp_cnt=60
+target_pid=
+target_logfile=$1_ctxt_trend.log
+if [ -z "$1" ]
+  then
+    echo "Usage: $0 process_id "
+	exit
+fi
+
+target_pid=$1
+
+echo "target pid = $target_pid"
+echo "voluntary_ctxt_switches	nonvoluntary_ctxt_switches" 2>&1 | tee -a "$target_logfile"
+
+for ((i=0;i<$tmp_cnt;i++)); do
+	grep -E "ctxt" /proc/$target_pid/status | awk '{ print $2 }' | xargs -n2
+	sleep 1
+done
