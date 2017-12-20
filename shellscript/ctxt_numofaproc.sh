1ea038a559cc65d94a9cc378f43d8dcbb39206de
Bruce Kim <bruce.kim.it@gmail.com>
2017-12-20 10:04:39 +0900

    store the measurement into file
    store the measurement into file

--- shellscript/ctxt_numofaproc.sh
+++ shellscript/ctxt_numofaproc.sh
@@ -13,6 +13,6 @@ echo "target pid = $target_pid"
 echo "voluntary_ctxt_switches	nonvoluntary_ctxt_switches" 2>&1 | tee -a "$target_logfile"
 
 for ((i=0;i<$tmp_cnt;i++)); do
-	grep -E "ctxt" /proc/$target_pid/status | awk '{ print $2 }' | xargs -n2
+	grep -E "ctxt" /proc/$target_pid/status | awk '{ print $2 }' | xargs -n2 | tee -a "$target_logfile"
 	sleep 1
 done
